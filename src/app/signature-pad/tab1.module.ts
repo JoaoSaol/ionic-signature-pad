e47525261557b69d733f1d22a25d00bc2dfa42a7
JoaoSaol <joaosaol596@gmail.com>
2022-12-07 09:36:06 -0300

    remove module don't used of tab1

--- src/app/signature-pad/tab1.module.ts
+++ src/app/signature-pad/tab1.module.ts
@@ -4,10 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 
 import { Tab1PageRoutingModule } from './signature-pad-routing.module';
-import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 
 @NgModule({
   imports: [
